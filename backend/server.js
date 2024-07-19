159859f1463a683141263fa866f19a47305d65d6
NqobileNgobeni <115787995+NqobileNgobeni@users.noreply.github.com>
2024-07-19 20:30:27 +0200

    datatbase setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import { ENV_VARS } from "./config/config_env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
+const PORT = ENV_VARS.PORT;
 
 app.use("/api/v1/auth", authRoutes);
 
-const port = 5000;
-app.listen(port, console.log(`Listening to port : ${port}`));
+app.listen(PORT, () => {
+  console.log(`Listening to port : ${PORT}, running at http://localhost:5000`);
+  connectDB();
+});
