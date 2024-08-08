acfbf66bacae9907f6e59542e57c81591c5ea780
NqobileNgobeni <115787995+NqobileNgobeni@users.noreply.github.com>
2024-08-08 10:53:15 +0200

    New backendCode

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,38 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
 
-import protectRoute from './middleware/protectRoute.middleware.js';
-import authRoutes from './routes/auth.routes.js';
-import movieRoutes from './routes/movie.routes.js';
-import tvRoutes from './routes/tv.routes.js';
-import searchRoute from './routes/search.routes.js';
+import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
 
-import { ENV_VARS } from './config/config_env.js';
-import { connectDB } from './config/db.js';
-
-// Add these lines to create __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { ENV_VARS } from "./config/envVars.js";
+import { connectDB } from "./config/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
-const PORT = ENV_VARS.PORT;
 
-// middleware
-app.use(express.json());
+const PORT = ENV_VARS.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json()); // will allow us to parse req.body
 app.use(cookieParser());
 
-// routes
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/movie', protectRoute, movieRoutes);
-app.use('/api/v1/tv', protectRoute, tvRoutes);
-app.use('/api/v1/search', protectRoute, searchRoute);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
-// deployment config
-if (ENV_VARS.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-  });
+if (ENV_VARS.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
 app.listen(PORT, () => {
-  console.log(`Listening to port : ${PORT}, running at http://localhost:5000`);
-  connectDB();
+	console.log("Server started at http://localhost:" + PORT);
+	connectDB();
 });
